ae05a08f16839cfa65fda7e3bfb5a69c3624ddd4
IdaWati15 <idaw9765@gmail.com>
2022-10-28 20:59:43 +0700

    commit tipe data

---
+++ test/widget_test.dart
@@ -0,0 +1,64 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_pertamaa/main.dart';
+
+void main() {
+ //tipe data dan variabel 
+//var
+ var mahasiswa = "Jejen"; 
+ var umur = "20";
+
+ print(mahasiswa + "Umur = " + umur.toString()); 
+
+ //string
+ String mahasiswaString ;
+ mahasiswaString = "Aan";
+
+ print(mahasiswaString);
+
+//int
+int semester;
+semester = 6;
+
+print(semester);
+
+//double 
+double ipk;
+ipk = 3.8;
+
+print(ipk);
+
+//boolean 
+bool benar = true;
+bool salah = false;
+bool tidakbenar = !true;
+bool tidaksalah = !false;
+
+//List
+List jurusan = [
+  "Teknik Informatika", 
+  "Sistem Informasi",
+  semester.toString(),
+  ipk.toString()
+];
+
+print(jurusan);
+
+//Map
+Map kelas = {
+  "nama": "beben",
+  "kelas": "TI 2",
+};
+
+print(kelas);
+print(kelas['nama']);
+print(kelas['kelas']);
+}
